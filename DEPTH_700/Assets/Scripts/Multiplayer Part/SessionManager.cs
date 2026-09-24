d33b93f6c330c27aaf7412e93b5a1d9a2a898746
Bone-228 <kostia.mamai@gmail.com>
2026-09-24 18:03:55 +0200

    Session manager implementation
    now the session can be created with the code to room

--- DEPTH_700/Assets/Scripts/Multiplayer Part/SessionManager.cs
+++ DEPTH_700/Assets/Scripts/Multiplayer Part/SessionManager.cs
@@ -1,16 +1,29 @@
+using System;
 using UnityEngine;
+using Unity.Services.Multiplayer;
 
 public class SessionManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private ISession currentSession;
 
-    // Update is called once per frame
-    void Update()
+    public async void CreateSession()
     {
-        
+        try
+        {
+            var options = new SessionOptions
+            {
+                MaxPlayers = 4
+            }.WithRelayNetwork();
+
+            currentSession =
+                await MultiplayerService.Instance.CreateSessionAsync(options);
+
+            Debug.Log("Session created!");
+            Debug.Log($"Session Code: {currentSession.Code}");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to create session: {e}");
+        }
     }
 }
